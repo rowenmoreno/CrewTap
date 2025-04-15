cd7c316e8e3e1e63d8e9ff419997f1d308d50e43
Rowen Moreno <rjanmoreno@gmail.com>
2025-04-15 18:52:07 +0800

    fix index in connect screen

--- lib/screens/connect/connect_screen.dart
+++ lib/screens/connect/connect_screen.dart
@@ -20,7 +20,7 @@ class _ConnectScreenState extends State<ConnectScreen> with SingleTickerProvider
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: 3, vsync: this);
+    _tabController = TabController(length: 2, vsync: this);
     _loadUserProfile();
   }
 
